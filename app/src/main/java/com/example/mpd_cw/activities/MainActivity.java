cdb00ba02b7c643661fcb281add42c38437cf789
Cynthia Iradukunda <c.iradukund@alustudent.com>
2022-04-24 19:52:14 +0200

    added source

--- app/src/main/java/com/example/mpd_cw/activities/MainActivity.java
+++ app/src/main/java/com/example/mpd_cw/activities/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -61,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public boolean isNetworkAvailable(){
+        //The code from https://stackoverflow.com/questions/4238921/detect-whether-there-is-an-internet-connection-available-on-android
         ConnectivityManager connectivityManager = ((ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE));
         return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
 
